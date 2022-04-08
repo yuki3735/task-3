41b2781ab4fe6e0402acac88647203b5057a3847
yuki3735 <101887861+yuki3735@users.noreply.github.com>
2022-04-09 05:20:59 +0900

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,14 +1,17 @@
 let pointFlg ="false";//小数点の確認用
-let checkNum ="";//
+let checkNum ="";//文字か数字かの確認用
 let disVal = document.querySelector("input");
 
 function element (set) {
     //最初の入力が０または.の時
     if((disVal.value =="" && set =="0") || (disVal.value =="" && set ==".") || (disVal.value =="" && set =="00")){
         disVal.value +="0.";
-        pointFlg ="true";//小数点を使えないようにする
+    //小数点を使えないようにする 
+        pointFlg ="true";
+    //一つの項に小数点が複数つかないように
     }else if(pointFlg =="true" && set =="."){
         set ="";
+    //文字列の横に小数点がこないように
     }else {
         checkNum =isNaN(disVal.value.slice(-1));
         if((checkNum ==true) && (set ==".")){
@@ -20,8 +23,10 @@ function element (set) {
 }
 
 function calc(set) {
+    //マイナス以外は初手禁止
     if(disVal ==""&& set !="-"){
         return;
+    //連続した演算子の禁止
     }else {
         checkNum =isNaN(disVal.value.slice(-1));
         if(checkNum ==true){
@@ -40,8 +45,10 @@ function reset(set) {
 
 function equal(set) {
     try{
+    //小数点第8桁まで表示
         let keta =100000000;
         let total =eval(disVal.value);
+    //整数にできる部分を整数にして、それ以下は四捨五入にする
         total =Math.round(total*keta)/keta;
         disVal.value =total;
     }catch(error) {
